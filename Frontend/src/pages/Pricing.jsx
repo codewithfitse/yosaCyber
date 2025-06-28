1c03b524bb9c2815a9cfd3be1d57ab5975962a24
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-28 23:29:45 +0300

    Change on Pricing

--- Frontend/src/pages/Pricing.jsx
+++ Frontend/src/pages/Pricing.jsx
@@ -7,25 +7,54 @@ import axios from "axios";
 
 const Pricing = () => {
   return (
-    <div className="w-full h-[100%] background text-amber-50">
+    <div className="w-full h-full background text-amber-50">
       <Header />
-      <div className="w-full h-full mt-40 lg:mt-10 flex flex-col lg:flex-row">
-        <div className="w-full h-auto lg:pl-20 lg:pr-10">
+      <div className="w-full h-full pt-30 lg:mt-10 flex flex-col lg:flex-row bg-[#121212]">
+        <div className="w-full h-auto">
+          <div className="w-full h-full">
+            <img src="/image3.png" className="w-full h-full" alt="" srcset="" />
+          </div>
+        </div>
+        <div className="w-full h-fit py-15 pl-10 flex flex-col justify-center text-gray-300">
+          <h1 className="font-bold text-[25px] text-white">
+            WITH YOUR AUTHORITY
+          </h1>
+          <h2 className="w-[80%] mt-5 text-[20px]">
+            Prices for our truck dispatching services start form 7% gross
+            revenue and up depending on your equipment.
+          </h2>
+          <ul className="w-[80%] mt-5 pl-5 list-disc list-inside text-[20px] text-white space-y-1">
+            <li>Box Truck - 7%</li>
+            <li>Dry van - 10%</li>
+            <li>Reefer- 10%</li>
+          </ul>
+          <button className="Button">Contact Us</button>
+        </div>
+      </div>
+
+      <div className="w-full h-full mb-10 pt-5 lg:mt-10 flex flex-col lg:flex-row bg-black">
+        <div className="w-full h-auto">
           <div className="w-full h-full">
             <img src="/image3.png" className="w-full h-fit" alt="" srcset="" />
           </div>
         </div>
-        <div className="w-full h-fit flex flex-col justify-center items-center text-gray-300">
-          <h1 className="text-3xl text-center text-white">Flat Bed</h1>
-          <h1 className="px-8 text-[18px] text-center mt-3">
-            Flatbed trailers are essential in dispatching services, handling
-            oversized and heavy cargo efficiently. Their versatility makes
-            flatbed trailers indispensable assets in transportation networks.
-            Dawit Freighters ensures efficient coordination and timely delivery
-            of such freight, offering invaluable support to shippers and
-            carriers alike.
+        <div className="w-full h-fit pl-10 flex flex-col justify-center text-gray-300">
+          <h1 className="font-bold text-[25px] text-white">
+            WITH YOUR AUTHORITY
           </h1>
-          <button className="Button">Submit</button>
+          <h2 className="w-[80%] mt-5 text-[20px]">
+            Prices for freight services under our authority starts from 12 %
+            gross revenue and up. You will get :
+          </h2>
+          <ul className="w-[80%] mt-5 pl-5 list-disc list-inside text-[20px] text-white space-y-1">
+            <li>Our MC Authority</li>
+            <li>Insurance and Safety</li>
+            <li>DOT compliance</li>
+            <li>Dispatching services</li>
+            <li>IFTA filing</li>
+            <li>Document management and other benefits</li>
+          </ul>
+          <button className="Button">Contact us</button>
         </div>
       </div>
       <Footer />
