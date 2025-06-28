5e4c6aac65b2b4eda184c540aa107410b28f4fc9
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-29 00:45:56 +0300

    Change on Home page

--- Frontend/src/pages/Landing.jsx
+++ Frontend/src/pages/Landing.jsx
@@ -4,21 +4,26 @@ import { Button } from "../components/Button";
 
 export const Landing = () => {
   return (
-    <div className="w-full h-auto landingBg overflow-hidden">
-      <div className="h-screen pt-90 lg:pt-72 pl-5 lg:pl-15 pb-32 lg:pb-9 text-[25px]/7 text-white font-serif lg:text-6xl/15 font-bold backdrop-blur-[5px]">
-        <h1 className="mb-3 H1">
-          <span className="text-[80px] font-serif">Y</span>usa
-          <br />
-        </h1>
-        <h1 className="">Car Rental Website</h1>
-        <p className="text-white text-[15px] lg:text-[25px]/8 font-bold">
-          Find a variety of the car of your dreams, <br />
-          at a good price and quality
-        </p>
-        <div className="mt-10 flex justify-center">
-          <Link to="/Home">
-            <Button className="w-full" text={"Get Started"} />
-          </Link>
+    <div className="w-full h-auto overflow-hidden relative">
+      <div
+        className="h-screen pt-2 pl-5 lg:pl-15 text-[25px] text-white font-serif lg:text-6xl/15 font-bold backdrop-blur-[5px] 
+      "
+      >
+        <div className="absolute bottom-11">
+          <h1 className="mb-3 H1">
+            <span className="text-[80px] font-serif">Y</span>usa
+            <br />
+          </h1>
+          <h1 className="">Car Rental Website</h1>
+          <p className="text-white text-[15px] lg:text-[25px]/8 font-bold">
+            Find a variety of the car of your dreams, <br />
+            at a good price and quality
+          </p>
+          <div className="mt-10 flex justify-center">
+            <Link to="/Home">
+              <Button className="w-full" text={"Get Started"} />
+            </Link>
+          </div>
         </div>
       </div>
     </div>
