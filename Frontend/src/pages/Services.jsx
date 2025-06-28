6141351822d21685b83d28b506126f742fb34f37
Fitsum Zerihun <fitsa0909@gmail.com>
2025-06-28 16:58:33 +0300

    Update Services.jsx

--- Frontend/src/pages/Services.jsx
+++ Frontend/src/pages/Services.jsx
@@ -9,165 +9,90 @@ const Services = () => {
     <div className="w-full h-[100%] background text-amber-50">
       <Header />
 
-      <div className="pt-[100px]">
-        <h1 className="text-[40px] lg:text-[80px] text-center font-bold">
-          <span className="text-[#16fe01] ">Our</span> SERVICES
-        </h1>
-        <h2 className="text-[20px] lg:text-[40px] text-center">
-          WHAT WE DO BEST AT ADDIS CAR RENTAL
-        </h2>
-      </div>
-
-      <div className="mt-[60px] py-2 px-2 w-full grid lg:grid-cols-3 place-items-center gap-3 container">
-        <div className="w-full flex items-center py-5">
-          <div className="w-[20%] flex justify-center">
-            <div className="p-3 bg-[#16fe01] rounded-full">
-              <img src="Icons/car.png" className="w-10 h-10" alt="" srcset="" />
-            </div>
-          </div>
-          <div className="pl-1 w-[80%]">
-            <h1 className="pb-1 text-[40px] text-[#16fe01] font-bold">
-              CAR RENTAL
-            </h1>
-            <h2 className="w-[90%]">
-              Car Rentals in Addis Ababa with Daily, Weekly, and Monthly rates
-              for your Business, Visit, or Pleasure travel plans in Ethiopia.
-              See our selections here.
-            </h2>
-          </div>
-        </div>
-        <div className="w-full flex items-center py-5">
-          <div className="w-[20%] flex justify-center">
-            <div className="p-3 bg-[#16fe01] rounded-full">
-              <img
-                src="Icons/binoculars.png"
-                className="w-10 h-10"
-                alt=""
-                srcset=""
-              />
-            </div>
-          </div>
-          <div className="pl-1 w-[80%]">
-            <h1 className="pb-1 text-[40px] text-[#16fe01] font-bold">
-              CITY TOUR
-            </h1>
-            <h2 className="w-[90%]">
-              Full or Half Day Tours of Ethiopia’s capital city Addis Ababa on a
-              budget-friendly excursion. Visit religious and cultural sites with
-              us.
-            </h2>
-          </div>
-        </div>
-        <div className="w-full flex items-center py-5">
-          <div className="w-[20%] flex justify-center">
-            <div className="p-3 bg-[#16fe01] rounded-full">
-              <img src="Icons/map.png" className="w-10 h-10" alt="" srcset="" />
-            </div>
-          </div>
-          <div className="pl-1 w-[80%]">
-            <h1 className="pb-1 text-[40px]/10 text-[#16fe01] line-clamp-2 font-bold">
-              TRAVEL CONSULTING
-            </h1>
-            <h2 className="w-[90%]">
-              Are you planning to travel to Ethiopia and do you have some
-              questions? Contact with our travel experts for a free
-              consultation.
-            </h2>
-          </div>
-        </div>
-      </div>
-
-      <div className="pt-[100px]">
-        <h1 className="text-[40px] lg:text-[80px] text-center font-bold">
-          <span className="text-[#16fe01] ">Our</span> Promise
-        </h1>
-      </div>
-
-      <div className="mt-[60px] py-6 px-2 w-full grid lg:grid-cols-4 place-items-center gap-3 space-y-3 container">
-        <div className="w-full flex flex-col items-center border-l-5 border-l-[#16fe01] py-5">
-          <div className="flex justify-center">
-            <div className="p-2 bg-[#16fe01] rounded-full">
-              <img
-                src="Icons/verified.png"
-                className="w-10 h-10"
-                alt=""
-                srcset=""
-              />
-            </div>
-          </div>
-          <div className="pl-1 pt-1 flex justify-center">
-            <h1 className="pt-2 text-[30px]/8 text-center font-bold">
-              Good Price & Quality Service
-            </h1>
-          </div>
-        </div>
-        <div className="w-full flex flex-col items-center border-l-5 border-l-[#16fe01] py-5">
-          <div className="flex justify-center">
-            <div className="p-2 bg-[#16fe01] rounded-full">
-              <img
-                src="Icons/tools.png"
-                className="w-10 h-10"
-                alt=""
-                srcset=""
-              />
-            </div>
-          </div>
-          <div className="pl-1 pt-1 flex justify-center">
-            <h1 className="pt-2 text-[30px]/8 text-center font-bold">
-              Constantly Maintaiend Cars
-            </h1>
-          </div>
-        </div>
-        <div className="w-full flex flex-col items-center border-l-5 border-l-[#16fe01] py-5">
-          <div className="flex  justify-center">
-            <div className="p-2 bg-[#16fe01] rounded-full">
-              <img
-                src="Icons/infinity.png"
-                className="w-10 h-10"
-                alt=""
-                srcset=""
-              />
-            </div>
-          </div>
-          <div className="pl-1 pt-1 flex justify-center">
-            <h1 className="pt-2 text-[30px]/8 text-center font-bold">
-              Unlimited Mileage
-            </h1>
-          </div>
-        </div>
-        <div className="w-full flex flex-col items-center border-l-5 border-l-[#16fe01] py-5">
-          <div className="flex  justify-center">
-            <div className="p-2 bg-[#16fe01] rounded-full">
-              <img
-                src="Icons/deal.png"
-                className="w-10 h-10"
-                alt=""
-                srcset=""
-              />
-            </div>
-          </div>
-          <div className="pl-1 pt-1 flex justify-center">
-            <h1 className="pt-2 text-[30px]/8 text-center font-bold">
-              Reliable Customer Support
-            </h1>
-          </div>
-        </div>
-      </div>
-
-      <div className="my-[40px] lg:mt-[200px] px-5 lg:px-20 flex justify-between items-center">
-        <div className="flex flex-col justify-center">
-          <h1 className="text-[15px] lg:text-[40px] font-bold">
-            COME & TRY OUR <span className="text-[#16fe01]">SERVICES</span>
+      <div className="w-full h-fit pb-5 pt-30 mb-2 bg-[#121212]">
+        <div className="w-full pt-5 pb-5">
+          <h1 className="text-center font-semibold text-[20px] lg:text-[40px] H1">
+            HONEY BEE XPRESS LLC IS LOOKING FOR CDL TRUCK DRIVERS!
           </h1>
-          <h2 className="text-[10px] lg:text-[20px] text-2xl">
-            We Always Have The Best Customer Services In Town
-          </h2>
         </div>
-        <div className="lg:mt-7 flex justify-center items-center">
-          <Link to={"/Booking"}>
-            <Button text={"Book Now!"} />
-          </Link>
+        <div className="w-full h-full flex flex-col lg:flex-row">
+          <div className="w-full h-full">
+            <div className="w-full h-full flex flex-col space-y-5 pl-5 lg:pl-20 pr-2">
+              <h1 className="text-[15px] lg:text-[25px] H1">
+                Ready to join the team? Drop us a line
+              </h1>
+              <input
+                type="text"
+                className="w-full py-5 px-2 bg-black rounded-[5px] pl-2 placeholder:pl-2"
+                placeholder="Name"
+              />
+              <input
+                type="text"
+                className="w-full py-5 px-2 bg-black rounded-[5px] pl-2 placeholder:pl-2"
+                placeholder="Email*"
+              />
+              <input
+                type="text"
+                className="w-full py-15 px-2 bg-black rounded-[5px] pl-2 placeholder:pl-2"
+                placeholder="Message"
+              />
+              <button className="w-full p-5 bg-black rounded-[5px] pl-2">
+                Submit
+              </button>
+
+              <h1 className="w-auto text-center text-[14px]">
+                This site is protected by reCAPTCHA and the Google{" "}
+                <span className="H1">Privacy Policy </span>
+                and <span className="H1">Terms of Service</span> apply.
+              </h1>
+            </div>
+          </div>
+          <div className="w-full h-full pt-10 lg:pt-1">
+            <div className="flex flex-col space-y-3 pl-10">
+              <div className="text-gray-400">
+                <h1 className="font-bold text-[25px]">Why join Honey Bee?</h1>
+                <ul className="w-[80%] pl-5 list-disc list-inside text-[20px] text-white space-y-1">
+                  <li>Constant routes nationwide</li>
+                  <li>Guaranteed stream of income</li>
+                  <li>
+                    Dedicated support system to walk you through every step of
+                    the way
+                  </li>
+                  <li>A company that promotes healthy work life balance</li>
+                </ul>
+              </div>
+              <div className="text-gray-400">
+                <h1 className="font-bold text-[25px]">
+                  What you need to have:
+                </h1>
+                <ul className="w-[80%] pl-5 list-disc list-inside text-[20px] text-white space-y-1">
+                  <li>Valid CDL license</li>
+                  <li>1 year of driving experience</li>
+                  <li>1 year of driving experience</li>
+                </ul>
+              </div>
+              <div className="text-gray-400">
+                <h1 className="font-bold text-[25px]">
+                  What we'll need from you:
+                </h1>
+                <ul className="w-[80%] pl-5 list-disc list-inside text-[20px] text-white space-y-1">
+                  <li>
+                    What we'll need from you: Clear copy of your CDL- A (Front
+                    and Back)
+                  </li>
+                  <li>
+                    Clear copy of your MVR Record in PDF format DOT Medical
+                  </li>
+                  <li>DOT Medical Certificate</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+
+      <div className="w-full h-full">
+        
       </div>
 
       <Footer />
